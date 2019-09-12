105f0c189be49edc5d7b6bac4ec791846a36e073
Binay Kumar <bkumar10@sapient.com>
2019-09-12 18:46:34 +0530

    camera change

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,7 @@ var start=function(){
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+          scanner.start(cameras[1]);
         } else {
           console.error('No cameras found.');
         }
